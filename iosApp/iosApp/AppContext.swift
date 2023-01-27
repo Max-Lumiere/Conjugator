4af4813a2350c66e33a10ac7aad0a861c59e7618
Max Svetlov <5808419+Max-Lumiere@users.noreply.github.com>
2023-01-27 23:59:34 +0200

    [ios] context has been moved to shared

--- iosApp/iosApp/AppContext.swift
+++ iosApp/iosApp/AppContext.swift
@@ -18,85 +18,10 @@
 
 import shared
 
-final class AppContext {
+final class AppContext: Context {
 
-    let conjugationsService: ConjugationsService
-    let tenseLocalizationService = LithuanianTenseLocalizationService()
-
-    let aTypeService: VerbFormsService = ATypeFormsService()
-    let reflexiveATypeService: VerbFormsService = ReflexiveATypeFormsService()
-
-    let iTypeService: VerbFormsService = ITypeFormsService()
-    let reflexiveITypeService: VerbFormsService = ReflexiveITypeFormsService()
-
-    let oTypeService: VerbFormsService = OTypeFormsService()
-    let reflexiveOTypeService: VerbFormsService = ReflexiveOTypeFormsService()
-    
-    let eTypeService: VerbFormsService = ETypeFormsService()
-    let reflexiveETypeService: VerbFormsService = ReflexiveETypeFormsService()
-
-    let futureFormsService: VerbFormsService = FutureFormsService()
-    let reflexiveFutureFormsService: VerbFormsService
-
-    let imperativeFormsService: VerbFormsService = ImperativeFormsService()
-    let reflexiveImperativeFormsService: VerbFormsService
-
-    let conditionalFormsService: VerbFormsService = ConditionalFormsService()
-    let reflexiveConditionalFormsService: VerbFormsService
-
-    let presentConjugationsService: ConjugationsService
-    let pastConjugationsService: ConjugationsService
-    let pastContiniousConjugationsService: ConjugationsService
-    let futureConjugationsService: ConjugationsService
-    let imperativeConjugationsService: ConjugationsService
-    let conditionalConjugationsService: ConjugationsService
-
-    init() {
-        reflexiveFutureFormsService = ReflexiveFutureFormsService(commonFormsService: futureFormsService)
-        reflexiveImperativeFormsService = ReflexiveImperativeFormsService(commonFormsService: imperativeFormsService)
-        reflexiveConditionalFormsService = ReflexiveConditionalFormsService(commonFormsService: conditionalFormsService)
-
-        presentConjugationsService = PresentConjugationsService(aTypeService: aTypeService,
-                                                                iTypeService: iTypeService,
-                                                                oTypeService: oTypeService,
-                                                                reflexiveATypeService: reflexiveATypeService,
-                                                                reflexiveITypeService: reflexiveITypeService,
-                                                                reflexiveOTypeService: reflexiveOTypeService,
-                                                                butiService: ButiPresentVerbFormsService())
-
-        pastConjugationsService = PastConjugationsService(oTypeService: oTypeService,
-                                                          eTypeService: eTypeService,
-                                                          reflexiveOTypeService: reflexiveOTypeService,
-                                                          reflexiveETypeService: reflexiveETypeService)
-
-        pastContiniousConjugationsService = PastContiniousConjugationsService(
-            oTypeService: oTypeService,
-            reflexiveOTypeService: reflexiveOTypeService
-        )
-
-        futureConjugationsService = FutureConjugationsService(commonFormsService: futureFormsService,
-                                                              reflexiveFormsService: reflexiveFutureFormsService,
-                                                              butiFormsService: ButiFutureVerbFormsService())
-
-        imperativeConjugationsService = ImperativeConjugationsServiceDecorator(inner: SimpleConjugationsService(
-            tense: .imperative,
-            commonFormsService: imperativeFormsService,
-            reflexiveFormsService: reflexiveImperativeFormsService
-        ))
-
-        conditionalConjugationsService = SimpleConjugationsService(
-            tense: .conditional,
-            commonFormsService: conditionalFormsService,
-            reflexiveFormsService: reflexiveConditionalFormsService
-        )
-
-        conjugationsService = CollectionConjugationsService(innerServices: [presentConjugationsService,
-                                                                            pastConjugationsService,
-                                                                            pastContiniousConjugationsService,
-                                                                            futureConjugationsService,
-                                                                            imperativeConjugationsService,
-                                                                            conditionalConjugationsService])
-
+    override init() {
+        super.init()
     }
 
 }
